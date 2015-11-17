698c80b17cd6fdaf82ee8c5e94c661c0fda22b3d
jplebre <jplebre@gmail.com>
2015-11-17 08:12:28 +0000

    working decimal

--- Week2/src/Calculator/CalculatorState.cs
+++ Week2/src/Calculator/CalculatorState.cs
@@ -2,6 +2,7 @@
 {
     internal class CalculatorState
     {
+
         public virtual CalculatorState Zero(Calculator calculator)
         {
             calculator.Display = "0";
@@ -17,12 +18,7 @@
         public virtual CalculatorState Decimal(Calculator calculator)
         {
             calculator.Display += ".";
-            // TODO: Change State When Decimal is pressed.
-            // Once a decimal point is used we don't want to allow any more
-            // So the state of the calculator changes.
-            // Implement this by returning an instance of 'EnteringDecimalNumber'
-            // Also: Complete the implementation of 'EnteringDecimalNumber' below
-            return new EnteringNumber();
+            return new EnteringDecimalNumber();
         }
 
         public virtual CalculatorState Clear(Calculator calculator)
@@ -39,8 +35,8 @@
 
         public virtual CalculatorState Negate(Calculator calculator)
         {
-            calculator.Display = "Not implemented";
-            return this;
+            calculator.Display = "-"+calculator.Display;
+            return new NegateState();
         }
 
         public virtual CalculatorState Add(Calculator calculator)
@@ -72,26 +68,31 @@
     {
         public override CalculatorState Zero(Calculator calculator)
         {
-            // TODO: Append '0' when entering a number.
-            // Override the base implementation which sets the display to 0
-            // Instead you should append 0 to the display
+            calculator.Display += "0";
             return this;
         }
 
         public override CalculatorState Digit(Calculator calculator, string digit)
         {
-            // TODO: Append the digit when entering a number.
-            // Override the base implementation which sets the display to the digit
-            // Instead you should append the digit to the display
+            calculator.Display += digit;
             return this;
         }
     }
 
     internal class EnteringDecimalNumber : EnteringNumber
     {
-        // TODO: Override the default implementation for handling Decimal Point
-        // Override the Decimal method to handle situations where a decimal point
-        // Has already been entered.
-        // This override should not append a decimal point.
+        public override CalculatorState Decimal(Calculator calculator)
+        {
+            return new EnteringDecimalNumber();
+        }
+    }
+
+    internal class NegateState : CalculatorState
+    {
+        public override CalculatorState Negate(Calculator calculator)
+        {
+            calculator.Display = calculator.Display.Remove(0, 1);
+            return new CalculatorState();
+        }
     }
 }
